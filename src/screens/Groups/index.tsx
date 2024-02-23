4963e4ca753b2084bd427e30820697597c8d61ea
MichaelTeste <ronaldmichael918@gmail.com>
2024-02-23 09:06:42 -0300

    listagem de grupos e criação do componente empty list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,28 @@
-import { Text } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import { Container, Title } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+    const [groups, setGroup] = useState<string[]>([]);
+
     return (
         <Container>
             <Header />
             <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-            <GroupCard title="A galera daqui" />
+
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => <GroupCard title={item} />}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Que tal cadastrar a primeira turma?" />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+            />
         </Container>
     );
 }
