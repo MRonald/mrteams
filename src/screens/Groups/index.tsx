35f12f02b10ba7838578a76b9795f8c169776d40
MichaelTeste <ronaldmichael918@gmail.com>
2024-02-23 08:51:07 -0300

    criando componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,12 +2,14 @@ import { Text } from 'react-native';
 import { Container, Title } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
     return (
         <Container>
             <Header />
             <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+            <GroupCard title="A galera daqui" />
         </Container>
     );
 }
